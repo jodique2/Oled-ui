59dee6cf4f2706cc74a2ea6dae6d2e59f556d109
jodique2 <ruimendes873@gmail.com>
2025-04-17 23:23:07 +0100

    Added diferent kinds of blinking and a loking around/daydreaming logic. Also changed some stuff so that it is easier to make mods in the future. Lastly made it all more modular so that i can reuse code.

--- src/main.cpp
+++ src/main.cpp
@@ -8,30 +8,78 @@
 #define OLED_RESET -1
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
-String input = "";
+unsigned long lastBlinkTime = 0;
+unsigned long lastTimeOffSet = 0;
+int offsetX = 0;
+int offsetY = 0;
+int targetOffsetX = 0;
+int targetOffsetY = 0;
+
+int centered_x = 34;
+int centered_y = 20;
+int blinkInterval = random(2500,4000);
 
 void setup() {
   Serial.begin(9600);
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
-  display.setTextSize(2); // Increased text size
-  display.setTextColor(SSD1306_WHITE);
-  display.setCursor(0, 0);
   display.display();
+}
+/*drawEyes will draw the eyes on the given x and y position*/
+void drawEyes(int x, int y) {
+  display.clearDisplay();
+  display.fillRoundRect(x, y, 20, 30, 5, SSD1306_WHITE);
+  display.fillRoundRect(x+40, y, 20, 30, 5, SSD1306_WHITE);
+  display.display();
+  delay(30);
+}
+/*drawBlink will blink once*/
+void drawBlink(int x, int y){
+  display.clearDisplay();
+  display.fillRoundRect(x,y+12,20,6,2,SSD1306_WHITE); /*the y+12 is because the greater the y, the lower in the screen you are*/
+  display.fillRoundRect(x+40,y+12,20,6,2,SSD1306_WHITE); /*the x+40 is to compensate the size of the other eye and a space between the eyes*/
+  display.display();
+  delay(50);
+}
+/*drawBlinkTwice will blink twice in a row*/
+void drawBlinkTwice(int x, int y){
+  drawBlink(x,y);
+  drawEyes(x,y);
+  drawBlink(x,y);
+}
+/*a random number between 1-3 is generated and is less likely to blinkTwice then once, because only blinksTwice when a == 3*/
+void getRandomBlink(int x, int y){
+  int a = random(1,4);
+  if(a == 3){
+    drawBlinkTwice(x,y);
+  }
+  else{
+    drawBlink(x,y);
+  }
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    input = Serial.readStringUntil('\n'); // Waits until Enter is pressed
-
-    display.clearDisplay();             // Clear previous text
-    display.setCursor(0, 0);            // Reset cursor
-    display.setTextSize(2);             // Bigger text size
-    display.setTextColor(SSD1306_WHITE);
-    display.println(input);            // Print input to display
-    display.display();                 // Update screen
-
-    Serial.print("Updated display with: ");
-    Serial.println(input);
+  if (millis() - lastTimeOffSet >= 2000){ /*the offset is only created with a 1 second interval*/
+    lastTimeOffSet = millis();
+    targetOffsetX = random(-3, 4);
+    targetOffsetY = random(-2, 3);
   }
+
+  /*targetOffSet ensures that the transition is smooth, so that it looks like it is loking around/daydreaming */
+  if (offsetX < targetOffsetX) offsetX++;
+  else if (offsetX > targetOffsetX) offsetX--;
+
+  if (offsetY < targetOffsetY) offsetY++;
+  else if (offsetY > targetOffsetY) offsetY--;
+
+
+  drawEyes(centered_x + offsetX, centered_y + offsetY);
+
+  if(millis() - lastBlinkTime >= blinkInterval){
+    blinkInterval=random(2500,4000); /*a diferent blink interval, will bring more variety*/
+    lastBlinkTime = millis();
+    getRandomBlink(centered_x,centered_y);
+  }
+
+  delay(30);
 }
